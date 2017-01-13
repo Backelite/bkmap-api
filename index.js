9eb28dc338af90afafda6fa9321a46e7b1cbc6f9
Laurent Janet <laurent.janet@backelite.com>
2017-01-13 11:34:09 +0100

    implements [POST] /beacons

--- index.js
+++ index.js
@@ -1,8 +1,46 @@
 const express = require('express');
 const app = express();
+const mongoClient = require('mongodb').MongoClient;
+const mongoDbPath = 'mongodb://localhost:27017/bkmap';
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
 
 app.get('/', function (request, response){
   response.send('bkmap');
+});
+
+/**
+ * Beacon creation
+ * {
+ *  uuid: string,
+ *  major: integer,
+ *  minor: integer,
+ *  position: {
+ *    x: integer,
+ *    y: integer,
+ *    stage: integer,
+ *  }
+ * }
+ *
+ */
+app.post('/beacons', function (request, response) {
+  const beacon = request.body;
+
+  mongoClient.connect(mongoDbPath, (err, db) => {
+    db.collection('beacons')
+      .insertOne(
+        beacon,
+        (error, result) => {
+          if (error) {
+            response.status(400).send({ err });
+          } else {
+            response.status(201).send(result);
+            db.close();
+          }
+        }
+      );
+  });
 });
 
 app.listen(3000);
